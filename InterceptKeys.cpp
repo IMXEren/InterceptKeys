eeac5227141346ffaa17ed266e90958033f1310c
IMXEren <96839938+IMXEren@users.noreply.github.com>
2025-05-04 06:04:39 +0530

    fix: Release LWin + LShift on the consecutive F23 key stroke, to not intefere with RCtrl strokes with combos

--- InterceptKeys.cpp
+++ InterceptKeys.cpp
@@ -2,6 +2,12 @@
 #include <windows.h>
 #include <stdio.h>
 
+#ifdef _DEBUG
+#define DEBUG_PRINT(...) printf(__VA_ARGS__)
+#else
+#define DEBUG_PRINT(...) 
+#endif
+
 #define SC_LWIN   0x5B
 #define SC_LSHIFT 0x2A
 #define SC_F23    0x6E
@@ -48,7 +54,7 @@ int main() {
 			int state = kstroke->state;
 
 			/*if (code == SC_LCTRL) {
-				printf("Pressed LCtrl: %d\n", state);
+				DEBUG_PRINT("Pressed LCtrl: %d\n", state);
 				InterceptionKeyStroke rctrl_stroke = { SC_LCTRL, state | INTERCEPTION_KEY_E0 };
 				interception_send(context, device, (InterceptionStroke*)&rctrl_stroke, 1);
 				continue;
@@ -59,18 +65,25 @@ int main() {
 			bool e0 = (state & INTERCEPTION_KEY_E0) != 0; // Extended key flag
 
 			if (code == SC_LWIN && e0) {
-				//printf("Pressed LWin: %d\n", is_down);
+				DEBUG_PRINT("Pressed LWin: %d\n", is_down);
 				lwin_down = is_down;
 			}
 			else if (code == SC_LSHIFT && !e0) {
-				//printf("Pressed LShift: %d\n", is_down);
+				DEBUG_PRINT("Pressed LShift: %d\n", is_down);
 				lshift_down = is_down;
 			}
 			else if (code == SC_F23 && !e0) {
-				//printf("Pressed F23: %d\n", is_down);
+				DEBUG_PRINT("Pressed F23: %d\n", is_down);
 
 				// Suppress F23
 				if (lwin_down && lshift_down) {
+
+					// Release LWin and LShift as to not combine them with RCtrl
+					InterceptionKeyStroke win_up_stroke = { SC_LWIN, INTERCEPTION_KEY_UP | INTERCEPTION_KEY_E0 };
+					InterceptionKeyStroke shift_up_stroke = { SC_LSHIFT, INTERCEPTION_KEY_UP };
+					interception_send(context, device, (InterceptionStroke*)&win_up_stroke, 1);
+					interception_send(context, device, (InterceptionStroke*)&shift_up_stroke, 1);
+
 					InterceptionKeyStroke rctrl_stroke;
 					rctrl_stroke.code = SC_LCTRL;
 					if (is_down) {
@@ -82,15 +95,13 @@ int main() {
 					rctrl_stroke.state |= INTERCEPTION_KEY_E0; // Set the extended key flag for RCTRL
 
 					interception_send(context, device, (InterceptionStroke*)&rctrl_stroke, 1);
-					//printf("Sending RCtrl: %d\n", is_down);
+					DEBUG_PRINT("Sending RCtrl: %d\n", is_down);
 					continue;
 				}
 
 			}
 			else {
-				//printf("Pressed key: %d, state: %d\n", code, state);
-				lwin_down = false;
-				lshift_down = false;
+				DEBUG_PRINT("Pressed key: %d, click: %d, state: %d\n", code, is_down, state);
 			}
 		}
 
