33e1480ce867270316c8fd5abf5461f0600ad130
IMXEren <96839938+IMXEren@users.noreply.github.com>
2025-05-27 00:01:34 +0530

    style: formatting

--- add_to_auto_start.ps1
+++ add_to_auto_start.ps1
@@ -1,17 +1,17 @@
 function Pass-Parameters {
     Param ([hashtable]$NamedParameters)
-    return ($NamedParameters.GetEnumerator()|%{"-$($_.Key) `"$($_.Value)`""}) -join " "
+    return ($NamedParameters.GetEnumerator() | ForEach-Object { "-$($_.Key) `"$($_.Value)`"" }) -join " "
 }
 
 # Self-elevate the script if required
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
- if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-  $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + (Pass-Parameters $MyInvocation.BoundParameters) + " " + $MyInvocation.UnboundArguments
-  Start-Process -FilePath pwsh.exe -Verb Runas -ArgumentList $CommandLine
-  Exit
- }
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+        $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + (Pass-Parameters $MyInvocation.BoundParameters) + " " + $MyInvocation.UnboundArguments
+        Start-Process -FilePath pwsh.exe -Verb Runas -ArgumentList $CommandLine
+        Exit
+    }
 }
 
 # Write-Host "Running as Administrator"
-python add_to_service.py
+# python add_to_service.py
 Pause
