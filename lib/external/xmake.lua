1121219531bc526b7a2e8bb3eb92cb670c968198
IMXEren <96839938+IMXEren@users.noreply.github.com>
2025-07-01 11:08:30 +0530

    chore: moving external dependencies to `lib/external`

---
+++ lib/external/xmake.lua
@@ -0,0 +1,4 @@
+includes("g3sinks") -- Not used
+includes("interception")
+includes("CLI11")
+includes("toml++")
--- lib/xmake.lua
+++ lib/xmake.lua
@@ -1,4 +1,1 @@
-includes("g3sinks") -- Not used
-includes("interception")
-includes("CLI11")
-includes("toml++")
+includes("external")
